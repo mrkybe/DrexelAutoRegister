d6c8aabd4adf7b5bd4ba50632d5ce4681d85919a
Ruslan Kaybyshev <rkay94@gmail.com>
2016-03-12 15:52:46 -0500

    -quickfix

--- WebApp/app/src/main/java/com/httpdowefeelthebern/cs275lab4webservices/MainActivity.java
+++ WebApp/app/src/main/java/com/httpdowefeelthebern/cs275lab4webservices/MainActivity.java
@@ -1,58 +1,24 @@
 package com.httpdowefeelthebern.cs275lab4webservices;
 
-import android.app.Service;
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.drawable.Drawable;
-import android.location.Address;
-import android.location.Criteria;
-import android.location.Geocoder;
-import android.location.GpsStatus;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Locale;
 import java.net.HttpURLConnection;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     TextView textViewResponse;
-    TextView textViewSent;
     EditText urlText;
     EditText userIDText;
     EditText userPasswordText;
@@ -63,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textViewResponse = (TextView)findViewById(R.id.textViewResponse);
-        textViewResponse = (TextView)findViewById(R.id.textViewSent);
         urlText = (EditText)findViewById(R.id.editTextURL);
         userIDText = (EditText)findViewById(R.id.editTextUserID);
         userPasswordText = (EditText)findViewById(R.id.editTextPassword);
@@ -108,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                     urlConnection.addRequestProperty("id", data.email);
                     urlConnection.addRequestProperty("email",data.email);
                     urlConnection.addRequestProperty("password",data.pass);
-                    responseString += urlConnection.toString();
+                    responseString = urlConnection.toString() + "\n\n";
                     //urlConnection.addRequestProperty("crns",data.crns);
                     //responseString = urlConnection.getErrorStream()
                     print("test");
